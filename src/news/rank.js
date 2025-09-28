656d4bd36bd02c3ce9e6bc51ca06f58e702c133d
choijuhwan8 <choijuhwan8@gmail.com>
2025-09-28 21:51:39 +0800

    Publish

--- src/news/rank.js
+++
@@ -1,7 +0,0 @@
-export function rankItems(items) {
-  return [...items].sort((a, b) => {
-    const aTime = new Date(a.publishedAt).getTime();
-    const bTime = new Date(b.publishedAt).getTime();
-    return bTime - aTime;
-  });
-}
